dd3f3169f0e73bcf7e4ebd761a35c0f3f31d053f
ifoxer <romanliseykin@gmail.com>
2019-04-01 22:44:21 +0300

    задание 1

--- src/main/java/Zadanie1.java
+++ src/main/java/Zadanie1.java
@@ -1,2 +1,17 @@
 public class Zadanie1 {
+
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=34;
+        int c = a+b;
+        System.out.println("Сумма двух чисел = "+ c);
+
+        if (c >= 0){
+            System.out.println("Число положительное");
+        }else{
+            System.out.println("Число отрицательное");
+
+        }
+    }
 }
