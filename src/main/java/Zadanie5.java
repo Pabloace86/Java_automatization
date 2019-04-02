8135d08ca3fcc3255cfd9952afdfb751b58f9001
ifoxer <romanliseykin@gmail.com>
2019-04-02 15:30:06 +0300

    Задание 5

---
+++ src/main/java/Zadanie5.java
@@ -0,0 +1,21 @@
+//Сделал двумя способами)
+public class Zadanie5 {
+    public static void main(String[] args) {
+
+     /*   int[] x = new int[] {5,6,4};
+        int a = x[2];
+        System.out.println("Число которое зачеркнули: "+a);
+        int b = x[0], c = x[1];
+        System.out.println("Вычитаем 4 делим на 10, получаем: "+b+""+c);
+        System.out.print("X равен: "+a+""+b+""+c);
+    */
+
+     int a = 564;
+     int b = 56;
+     int c = 10;
+
+     int umn = b*c;
+     int per = a - umn;
+        System.out.print("X равен:"+per+""+b);
+    }
+}
