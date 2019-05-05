16c73a4c0d41b3a2061c4f5def141fdead0939cf
ifoxer <romanliseykin@gmail.com>
2019-05-05 14:35:15 +0300

    Задание 14

---
+++ src/main/java/Zadanie14.java
@@ -0,0 +1,9 @@
+public class Zadanie14 {
+    public static void main(String[] args) {
+
+        String inf = "Информатика";
+        String form = inf.substring(2,7);
+        String tik = inf.substring(7,10);
+        System.out.println(form+" "+tik);
+    }
+}
